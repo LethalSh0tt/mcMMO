e0f13c40f0b60b1503e0863de5c34c8e83fde3a9
t00thpick1 <t00thpick1dirko@gmail.com>
2014-02-21 12:26:27 -0500

    Remove old unused Chunklet classes

--- src/main/java/com/gmail/nossr50/listeners/WorldListener.java
+++ src/main/java/com/gmail/nossr50/listeners/WorldListener.java
@@ -1,27 +1,17 @@
 package com.gmail.nossr50.listeners;
 
-import java.io.File;
-
 import org.bukkit.Chunk;
-import org.bukkit.World;
 import org.bukkit.block.BlockState;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.ChunkUnloadEvent;
 import org.bukkit.event.world.StructureGrowEvent;
-import org.bukkit.event.world.WorldInitEvent;
 import org.bukkit.event.world.WorldUnloadEvent;
 
 import com.gmail.nossr50.mcMMO;
-import com.gmail.nossr50.util.blockmeta.conversion.BlockStoreConversionMain;
 
 public class WorldListener implements Listener {
-    private final mcMMO plugin;
-
-    public WorldListener(final mcMMO plugin) {
-        this.plugin = plugin;
-    }
 
     /**
      * Monitor StructureGrow events.
@@ -37,24 +27,6 @@ public class WorldListener implements Listener {
         for (BlockState blockState : event.getBlocks()) {
             mcMMO.getPlaceStore().setFalse(blockState);
         }
-    }
-
-    /**
-     * Monitor WorldInit events.
-     *
-     * @param event The event to watch
-     */
-    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-    public void onWorldInit(WorldInitEvent event) {
-        World world = event.getWorld();
-
-        if (!new File(world.getWorldFolder(), "mcmmo_data").exists() || plugin == null) {
-            return;
-        }
-
-        plugin.getLogger().info("Converting block storage for " + world.getName() + " to a new format.");
-
-        new BlockStoreConversionMain(world).run();
     }
 
     /**
